c8b4259a3aaf48c6a9d7807f3bea3e0545fb9079
Francesco <76690900+MrZukasa@users.noreply.github.com>
2021-12-07 22:28:10 +0100

    rebuild

---
+++ app.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: 'Vue',
+        mioArray: ['leando','luigi','mario'],
+        car: {
+          colore: 'red',
+          ruote:'quattro',
+          autoradio:'si'
+        },
+        linkImg : 'https://assets.codepen.io/t-1003/internal/avatars/teams/default.png?fit=crop&format=auto&height=256&version=1513627136&width=256',
+        linkImg2 : 'https://assets.codepen.io/t-1003/internal/avatars/teams/default.png?fit=crop&format=auto&height=256&version=1513627136&width=256',
+        myhtml: '<b>Bomberone</b>'
+    },
+    methods: {
+        cambioImg(){
+            this.linkImg2='https://cdn.iconscout.com/icon/free/png-256/react-1-282599.png'
+        }
+    }
+})
