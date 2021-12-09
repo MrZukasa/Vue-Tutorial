f1233b0b69a271f44073cd5182afdf225e8145d8
Francesco <76690900+MrZukasa@users.noreply.github.com>
2021-12-09 15:50:59 +0100

    End of the intro

--- app.js
+++ app.js
@@ -2,19 +2,38 @@ var app = new Vue({
     el: '#app',
     data: {
         name: 'Vue',
-        mioArray: ['leando','luigi','mario'],
+        mioArray: ['leando','luigi','mario'],        
         car: {
           colore: 'red',
           ruote:'quattro',
           autoradio:'si'
-        },
+        },        
         linkImg : 'https://assets.codepen.io/t-1003/internal/avatars/teams/default.png?fit=crop&format=auto&height=256&version=1513627136&width=256',
         linkImg2 : 'https://assets.codepen.io/t-1003/internal/avatars/teams/default.png?fit=crop&format=auto&height=256&version=1513627136&width=256',
-        myhtml: '<b>Bomberone</b>'
+        myhtml: '<b>Bomberone</b>'        
     },
     methods: {
         cambioImg(){
             this.linkImg2='https://cdn.iconscout.com/icon/free/png-256/react-1-282599.png'
-        }
+        }        
+    }
+})
+
+var ciclo = new Vue({
+    el:'#ciclo',
+    data: {
+        dogs:[
+            {race:'barboncino'},
+            {race:'volpino'},
+            {race:'pastore tedesco'}
+        ],
+        cats:['persiano','bengala','siamese']
+    }
+})
+
+var conto = new Vue({
+    el:'#conto',
+    data:{
+        counter: 0
     }
 })
